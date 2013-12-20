1cc7f97c5f6f84335d75ed71c4a322a338b006e4
Tim Hartmann <tfhartmann@gmail.com>
2013-12-20 13:52:32 -0500

    Clean up syntax

--- dnsadmin53/core.py
+++ dnsadmin53/core.py
@@ -1,6 +1,15 @@
 import boto
 
 
+def dottify(string):
+    """
+    Add a final period to DNS Name Strings.
+    """
+    if not string.endswith('.'):
+        string = string + '.'
+    return string
+
+
 class Route53:
     """
     dnsadmin for AWS Route 53 Core Methods
@@ -21,12 +30,13 @@ class Route53:
             print e
         return self.zones
 
-    def get_zone(self, dns_zone = 'devel.huit.harvard.edu'):
+    def get_zone(self, dns_zone):
         """
         Get Zone Object. Valid Param is FQDN.
         """
         try:
-            self.zone = self.r53.get_zone(dns_zone+'.')
+            dns_zone = dottify(dns_zone)
+            self.zone = self.r53.get_zone(dns_zone)
         except boto.route53.exception.DNSServerError as e:
             print e
         return self.zone
