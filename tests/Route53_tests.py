ebf9d995c727f4df5bff7f86d289734ddba4a094
Tim Hartmann <tfhartmann@gmail.com>
2013-12-24 11:16:02 -0500

    Updated Route53 tests with moto

--- tests/Route53_tests.py
+++ tests/Route53_tests.py
@@ -4,6 +4,9 @@ from moto import mock_route53
 
 @mock_route53
 def test_get_zone():
+    conn = boto.connect_route53()
+    conn.create_hosted_zone('devel.huit.harvard.edu.')
+
     r53 = core.Route53()
     zone = r53.get_zone('devel.huit.harvard.edu') 
     assert zone.name == 'devel.huit.harvard.edu.'
