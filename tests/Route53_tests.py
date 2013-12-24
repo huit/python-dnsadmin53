4021b5b482a1a98eed22213a782c14115b2503da
Tim Hartmann <tfhartmann@gmail.com>
2013-12-24 16:40:12 -0500

    Added new unit test

--- tests/Route53_tests.py
+++ tests/Route53_tests.py
@@ -1,4 +1,6 @@
 import boto
+import sure
+from nose import tools
 from dnsadmin53 import core
 from moto import mock_route53
 
@@ -11,3 +13,14 @@ def test_get_zone():
     r53 = core.Route53()
     zone = r53.get_zone('devel.huit.harvard.edu')
     assert zone.name == 'devel.huit.harvard.edu.'
+
+
+@mock_route53
+def test_list_zone():
+    conn = boto.connect_route53()
+    conn.create_hosted_zone('devel1.huit.harvard.edu.')
+    conn.create_hosted_zone('devel2.huit.harvard.edu.')
+
+    r53 = core.Route53()
+    zones = r53.list_zones()
+    len(zones).should.equal(2)
