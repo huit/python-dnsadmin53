d6eb478eda066fdb9a8febb90e382acb390771f6
Tim Hartmann <tfhartmann@gmail.com>
2014-01-10 12:48:49 -1000

    Added a test for get_zone_id

--- tests/Route53_tests.py
+++ tests/Route53_tests.py
@@ -35,3 +35,12 @@ def test_does_zone_exist():
     result = r53.does_zone_exist('devel.huit.harvard.edu')
     print result
     tools.assert_true(result)
+
+@mock_route53
+def test_get_zone_id():
+    conn = boto.connect_route53()
+    conn.create_hosted_zone('devel.huit.harvard.edu.')
+
+    r53 = core.Route53()
+    zone_id = r53.get_zone_id('devel.huit.harvard.edu')
+    tools.assert_true(zone_id)
